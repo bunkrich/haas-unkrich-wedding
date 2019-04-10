6bea675eda014a440ea66bb2486c2947d37deb1d
Brent Unkrich <bunkrich@gmail.com>
2019-04-10 13:02:38 -0400

    updated hotel info

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var houseNumberButton = document.getElementById("house-number-lookup");
-var onePersonLookup = [30, 40, 102, 165, 215, 313, 412, 419, 524, 712, 720, 828, 841, 910, 1062, 1506, 2766, 3544, 3900, 4637, 4719, 8501, 12214];
-var familyLookup = [209, 374, 401, 424, 977, 1005, 1062, 1144, 1321, 2503, 3160, 3400, 3439, 3753, 3838, 5183, 7027, 7483, 23437];
+var onePersonLookup = [30, 40, 102, 103, 165, 215, 313, 412, 419, 524, 712, 720, 828, 841, 910, 1062, 1506, 2766, 3544, 3900, 4637, 4719, 8501, 12214];
+var familyLookup = [209, 374, 401, 977, 1005, 1062, 1144, 1321, 2267, 2503, 3160, 3400, 3439, 3753, 3838, 5183, 7027, 7483, 23437];
 houseNumberButton.addEventListener("click", lookup);
 document.getElementById('house-number').addEventListener('keypress', function (event) {
   if (event.keyCode == 13) {
