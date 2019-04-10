946e681151a6eab277bec4844691ed91ca56aa6f
Brent Unkrich <bunkrich@gmail.com>
2019-04-10 00:46:36 -0400

    refac

--- js/rsvp.js
+++ js/rsvp.js
@@ -1,5 +1,5 @@
 
-    document.getElementById("mainNav").addEventListener("click", hideMenu);
-    function hideMenu() {
-        document.getElementById("menu-btn").checked = false;
-    }
+document.getElementById("mainNav").addEventListener("click", hideMenu);
+function hideMenu() {
+    document.getElementById("menu-btn").checked = false;
+}
